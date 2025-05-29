b61160624fb46f0037bcd558dc71e8a74665133d
Ganesh n bambulage <158245099+Ganesh7846@users.noreply.github.com>
2025-05-29 18:00:15 +0530

    Add files via upload

---
+++ reverse_string_using_concatination.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String st = "ganesh";
+	    String s = "";
+	    
+	    for (int i=st.length()-1;i>=0 ;i-- ){
+	        char ch = st.charAt(i);
+	        String d = Character.toString(ch);
+	        s = s.concat(d);
+	    } 
+		System.out.println("Hello World");
+		System.out.println(st);
+		System.out.println(s);
+	
+		
+	}
+}
